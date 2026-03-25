9835380174316dead5d2212e3c83cc05f06bdba3
Koh, Junsik <woori753939@gmail.com>
2026-03-25 10:31:02 +0900

    Hello 클래스 수정

--- src/git_test2/Hello.java
+++ src/git_test2/Hello.java
@@ -1,10 +1,14 @@
 package git_test2;
 
 public class Hello {
-	
+
 	String message = "안녕하세요";
-	
+
+	public Hello(String message) {
+		this.message = message;
+	}
+
 	public void say() {
-		System.out.println(message); 
+		System.out.println(message);
 	}
 }
